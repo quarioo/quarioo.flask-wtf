d24f99b2ff1b547d5ef9a126e31c94878bee8bcc
quarioo <kirillzubrilin.zzz@gmail.com>
2023-02-09 17:35:39 +0300

    problem №2 solve

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index/<page_title>')
-def index(page_title):
-    return render_template('base.html', title=page_title)
+@app.route('/training/<prof>')
+def index(prof: str):
+    if 'инженер' in prof.lower() or 'строитель' in prof.lower():
+        return render_template('base.html', prof='Инженерные тренажеры')
+    return render_template('base.html', prof='Научные симуляторы')
 
 
 if __name__ == '__main__':
